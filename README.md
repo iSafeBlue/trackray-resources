77c1438c24724ed98b33e53dfc37352fede7e5cc
浅蓝 <blue@ixsec.org>
2020-04-06 21:55:39 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # trackray-resources
 TrackRay 的资源文件项目
+
+
+
+将该项目下载重命名为 “resources”文件夹，并移动到trackray主程序目录下。
+
---
+++ python/test.py
@@ -0,0 +1,9 @@
+import urllib2
+import socket
+
+def params():
+    return {"url":"this is target url."}
+
+def verify(args):
+
+    return args
